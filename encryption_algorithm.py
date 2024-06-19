61e1b6ccb5302c7979ac4fc000c5dea6863722a9
Chukwunazaekpere <ceo.naza.tech@gmail.com>
2024-06-19 16:40:24 +0100

    encryption algorithm

--- encryption_algorithm.py
+++ encryption_algorithm.py
@@ -12,10 +12,10 @@ class EncryptionAlgorithm:
         self.encrypted_message_file = "./files/encrypted_message.txt"
         self.plain_message = plain_message_file_name
         self.secret_key_file_name = "./files/secret_key.txt" 
-        self.secret_key = None 
-        self.primitive_root = None
-        self.public_key = None 
-        self.large_prime = None 
+        self.secret_key = "" 
+        self.primitive_root = ""
+        self.public_key = "" 
+        self.large_prime = "" 
 
 
     def _file_helper(self, file_name:str, file_mode:str, content=""):
@@ -53,9 +53,15 @@ class EncryptionAlgorithm:
     def _secret_key_generator(self):
         public_key_content = self._file_helper(file_mode="rl",file_name=self.public_key_file_name)
         large_prime = self._get_line_prop(public_key_content, "prime")
-        public_key = self._get_line_prop(public_key_content, "generator public")
+        print("\n\t large_prime: ", large_prime)
+
+        public_key = self._get_line_prop(public_key_content, "public")
+        print("\n\t public_key: ", public_key)
+
         primitive_root = self._get_line_prop(public_key_content, "primitive")
-        public_key_power = math.pow(public_key, self.private_key)
+        print("\n\t primitive_root: ", primitive_root)
+
+        public_key_power = math.pow(float(public_key), float(self.private_key))
         secret_key = public_key_power % large_prime 
         self._file_helper(self.public_key_file_name, f"\n\t Secret Key:\n {str(secret_key)}")
         self.secret_key = secret_key
@@ -75,7 +81,8 @@ class EncryptionAlgorithm:
     def encrypt_message(self):
         self._encode_plaintext_chars() # Encode characters in plain text
         encoded_message_file = self._file_helper(file_mode="r", file_name=self.encoded_message_file_name)
-
+        self._generate_encrypter_public_key()
+        print("\n\t secret-key: ", self.secret_key)
         for encoded_char in encoded_message_file:
             encrypted_char = self.secret_key*encoded_char % self.large_prime
             self._file_helper(file_mode="a+", file_name=self.encrypted_message_file, content=encrypted_char)
