9243216be0e0a5e357342b88f5de07680079ced7
Chukwunazaekpere <ceo.naza.tech@gmail.com>
2024-06-19 22:43:15 +0100

    encryption algorithm

--- encryption_algorithm.py
+++ encryption_algorithm.py
@@ -28,6 +28,9 @@ class EncryptionAlgorithm:
             with open(file_name, "r")as file_content:
                 file_content_list = file_content.readlines()
             return file_content_list
+        elif file_mode == "w":
+            with open(file_name, "w")as file_handle:
+                file_handle.write(str(content))
         else:
             with open(file_name, "+a")as file_handle:
                 file_handle.write(str(content))
@@ -41,15 +44,6 @@ class EncryptionAlgorithm:
                     return l
                     # print("\n\t GF: ", l)
     
-    def _encode_plaintext_chars(self):
-        plain_text = self._file_helper(file_mode="r", file_name=self.plain_message)
-        for char in plain_text:
-            char_to_write = char
-            if char != " ":
-                char_to_code = ord(char)
-                char_to_write = char_to_code
-            self._file_helper(file_mode="a+", file_name=self.encoded_message_file_name, content=char_to_write)
-    
     def _secret_key_generator(self):
         public_key_content = self._file_helper(file_mode="rl",file_name=self.public_key_file_name)
         large_prime = self._get_line_prop(public_key_content, "prime")
@@ -60,14 +54,20 @@ class EncryptionAlgorithm:
 
         primitive_root = self._get_line_prop(public_key_content, "primitive")
         print("\n\t primitive_root: ", primitive_root)
+        print("\n\t self.private_key: ", self.private_key)
 
-        public_key_power = math.pow(float(public_key), float(self.private_key))
-        secret_key = public_key_power % large_prime 
+        pub_key_int = int(public_key.split(".")[0])
+        large_prime_int = int(large_prime.split(".")[0])
+        primitive_root_int = int(primitive_root.split(".")[0])
+
+        public_key_power = math.pow(pub_key_int, self.private_key)
+        secret_key = public_key_power % large_prime_int
+        secret_key_int = int(str(secret_key).split(".")[0])
         self._file_helper(self.public_key_file_name, f"\n\t Secret Key:\n {str(secret_key)}")
-        self.secret_key = secret_key
-        self.public_key = public_key
-        self.large_prime = large_prime
-        self.primitive_root = primitive_root
+        self.secret_key = secret_key_int
+        self.public_key = pub_key_int
+        self.large_prime = large_prime_int
+        self.primitive_root = primitive_root_int
 
     def _generate_encrypter_public_key(self):
         self._secret_key_generator()
@@ -79,17 +79,24 @@ class EncryptionAlgorithm:
 
 
     def encrypt_message(self):
-        self._encode_plaintext_chars() # Encode characters in plain text
         encoded_message_file = self._file_helper(file_mode="r", file_name=self.encoded_message_file_name)
         self._generate_encrypter_public_key()
-        print("\n\t secret-key: ", self.secret_key)
-        for encoded_char in encoded_message_file:
-            encrypted_char = self.secret_key*encoded_char % self.large_prime
-            self._file_helper(file_mode="a+", file_name=self.encrypted_message_file, content=encrypted_char)
+        print("\n\t encoded_char-key: ", self.secret_key)
+        print("\n\t encoded_message_file: ", encoded_message_file)
+        plain_text = self._file_helper(file_mode="r", file_name=self.plain_message)
+        self._file_helper(file_mode="w", file_name=self.encrypted_message_file, content="")
+        for char in plain_text:
+            char_to_write = str(char)
+            if char != " ":
+                char_to_write = ord(char)
+                # print("\n\t encoded_char-key: ", encoded_char)
+                encrypted_char = self.secret_key*char_to_write % self.large_prime
+                char_to_write = encrypted_char
+            self._file_helper(file_mode="a+", file_name=self.encrypted_message_file, content=f"{char_to_write}-")
         
 
 plain_message_file_name = "./files/plain_message.txt"
-private_key = 200
+private_key = 27
 ff = EncryptionAlgorithm(plain_message_file_name=plain_message_file_name, private_key=private_key)
 gf = ff.encrypt_message()
 
